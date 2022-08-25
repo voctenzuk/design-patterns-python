9dc7318448ad943d9b23d9ae3e955a135bc42122
Anton Kuznetsov <nyafish@gmail.com>
2022-08-25 19:19:42 +0300

    Add heat index display

--- observer/displays.py
+++ observer/displays.py
@@ -79,3 +79,40 @@ class ForecastDisplay(Observer, DisplayElement):
             print("More of the same")
         else:
             print("Watch out for cooler, rainy weather")
+
+
+class HeatIndexDisplay(Observer, DisplayElement):
+    _heatindex: float
+
+    def __init__(self, weather_data: WeatherData) -> None:
+        self.weather_data = weather_data
+        self.weather_data.register_observer(self)
+
+    def update(self) -> None:
+        self._heatindex = self.compute_heatindex(
+            self.weather_data.temperature, self.weather_data.humidity
+        )
+        self.display()
+
+    def display(self) -> None:
+        print(f"Heat index is {self._heatindex:.4f}")
+
+    @staticmethod
+    def compute_heatindex(t: float, rh: float):
+        return (
+            (16.923 + (0.185212 * t))
+            + (5.37941 * rh)
+            - (0.100254 * t * rh)
+            + (0.00941695 * (t * t))
+            + (0.00728898 * (rh * rh))
+            + (0.000345372 * (t * t * rh))
+            - (0.000814971 * (t * rh * rh))
+            + (0.0000102102 * (t * t * rh * rh))
+            - (0.000038646 * (t * t * t))
+            + (0.0000291583 * (rh * rh * rh))
+            + (0.00000142721 * (t * t * t * rh))
+            + (0.000000197483 * (t * rh * rh * rh))
+            - (0.0000000218429 * (t * t * t * rh * rh))
+            + (0.000000000843296 * (t * t * rh * rh * rh))
+            - (0.0000000000481975 * (t * t * t * rh * rh * rh))
+        )
